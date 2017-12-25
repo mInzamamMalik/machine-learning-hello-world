af2faa287b104282839b882eab8752ba62920fa7
Inzamam Malik <malikasinger1@gmail.com>
2017-12-25 22:13:42 +0500

    improvement

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import json
 
 from flask import Flask
 from flask import request
@@ -6,13 +7,25 @@ from flask import request
 from sklearn.datasets import load_iris
 from sklearn import tree
 
-import json
 app = Flask(__name__)
+
+
+def trainML():
+    print("Training Started!")
+    iris = load_iris()
+    # print(iris.data[60])
+    # print(iris.target[60])
+    clf = tree.DecisionTreeClassifier()
+    clf = clf.fit(iris.data, iris.target)
+    print("Training Completed!")
+    return clf
+
+
+clf = trainML()
 
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
-
     body = request.get_json()
     print("body: ", body)
 
@@ -20,37 +33,26 @@ def webhook():
     print("data: ", data)
 
     sLength = data['sLength']
-    pLength = data['pLength']
     sWidth = data['sWidth']
+    pLength = data['pLength']
     pWidth = data['pWidth']
 
-    result = guessFlower(sLength, pLength, sWidth, pWidth)
+    result = clf.predict([[sLength, sWidth, pLength, pWidth]])
 
-
+    print("result: ", result)
     head = ["Iris-setosa", "Iris-versicolor", "Iris-virginica"]
 
     return json.dumps({
-        "speech": "this is " + head[result[0]]
+        "speech": "<speak> " +
+                  "<s> Ok you said </s> " +
+                  "<s> sepal length is  " + sLength + "cm </s>" +
+                  "<s> sepal width is  " + sWidth + "cm </s>" +
+                  "<s> petal length is  " + pLength + "cm </s>" +
+                  "<s> and </s>" +
+                  "<s> petal width is  " + pWidth + "cm. </s>" +
+                  "<s> it looks like this flower belongs from " + head[result[0]] + " </s>" +
+                  "</speak>"
     })
-
-
-
-def trainML():
-    iris = load_iris()
-    print(iris.data[60])
-    print(iris.target[60])
-
-    clf = tree.DecisionTreeClassifier()
-    clf = clf.fit(iris.data, iris.target)
-    print("Training Done!")
-    return clf
-
-
-clf = trainML()
-
-
-def guessFlower(sLength, pLength, sWidth, pWidth):
-    return clf.predict([[sLength, pLength, sWidth, pWidth]])
 
 
 if __name__ == '__main__':
